56b6ca4fea355f00a71def8caab496335d02dea4
Daniel Pradera <Daniel Pradera>
2016-03-14 12:34:47 +0100

    Método remove()

--- MiHashSet.java
+++ MiHashSet.java
@@ -18,6 +18,10 @@ public class MiHashSet
         list = new ArrayListInt();
     }
 
+    /**
+     * añade el elemento al conjunto si no estaba. Devuelve verdadero en caso de que el elemento no estuviera 
+     * presente en el conjunto y falso en caso contrario.
+     */
     public boolean add(int valor)
     {
         boolean rep = list.contains(valor);
@@ -28,29 +32,54 @@ public class MiHashSet
         return !rep;
     }
     
+    /**
+     * vacía el conjunto.
+     */
     public void clear()
     {
         list.clear();
     }
     
+    /**
+     * devuelve verdadero si el conjunto contiene el elemento, falso en caso contrario.
+     */
     public boolean contains(int elemento)
     {
         return list.contains(elemento);
     }
     
-    public void isEmpty()
+    /**
+     * devuelve verdadero si el conjunto no contiene elementos.
+     */
+    public boolean isEmpty()
     {
-        list.isEmpty();
+        return list.isEmpty();
     }
     
-    public void remove(int elemento)
+    /**
+     * elimina del conjunto el elemento dado. Si no existiera devuelve falso; si existía en el conjunto
+     * devuelve verdadero.
+     */
+    public boolean remove(int elemento)
     {
+        boolean rep = list.contains(elemento);
+        if(rep)
+        {
+            list.remove(list.indexOf(elemento));
+        }
+        return rep;
     }
     
+    /**
+     * devuelve el número de elementos del conjunto.
+     */
     public void size()
     {
     }
     
+    /**
+     * devuelve verdadero si el parámetro es igual al conjunto sobre el que se invoca y falso en otro caso.
+     */
     public void equals(MiHashSet otroConjunto)
     {
     }
